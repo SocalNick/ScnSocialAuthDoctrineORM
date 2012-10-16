be1502bb7da431721c4ac730e806512ee6d02af3
Jérémy Huet <jeremy.huet@gmail.com>
2012-10-16 12:21:51 +0200

    Module base files.

---
+++ src/ScnSocialAuthDoctrineORM/Module.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace ScnSocialAuthDoctrineORM;
+
+class Module
+{
+    public function onBootstrap($e)
+    {
+    }
+
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\ClassMapAutoloader' => array(
+                __DIR__ . '/../../autoload_classmap.php',
+            ),
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__,
+                ),
+            ),
+        );
+    }
+
+    public function getServiceConfig()
+    {
+        return array(
+
+        );
+    }
+
+    public function getConfig()
+    {
+        return include __DIR__ . '/../../config/module.config.php';
+    }
+}
