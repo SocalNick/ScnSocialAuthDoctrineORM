263d317794d90b60075710de8abcdf71f80efde9
Jérémy Huet <jeremy.huet@gmail.com>
2012-10-18 16:28:04 +0200

    Overriding ModuleOptionsFactory to force the use of a Doctrine2 entity for UserProvider

--- config/module.config.php
+++ config/module.config.php
@@ -2,6 +2,7 @@
 return array(
     'service_manager' => array(
         'factories' => array(
+            'ScnSocialAuth-ModuleOptions' => 'ScnSocialAuthDoctrineORM\Service\ModuleOptionsFactory',
             'ScnSocialAuth-UserProviderMapper' => 'ScnSocialAuthDoctrineORM\Service\UserProviderMapperFactory',
         ),
     ),
