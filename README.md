0c9eda1b8f8a1946a82a48c67625622395d103d6
Tom Anderson <tanderson@solutionstream.com>
2015-05-05 15:16:31 -0600

    Default entity is toggleable through config

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ ScnSocialAuthDoctrineORM
 ========================
 
 An extension of [ScnSocialAuth](https://github.com/SocalNick/ScnSocialAuth) that provides integration with Doctrine2 ORM.
+
+
+Configuration
+-------------
+
+An entity is provided for UserProvider.  Based on the setting of 
+`$config['scn-social-auth']['enable_default_entities']` will the included entity
+be used.  To override the entity use 
+`$config['scn-social-auth']['user_provider_entity_class']`
--- config/module.config.php
+++ config/module.config.php
@@ -1,18 +1,5 @@
 <?php
 return array(
-    'doctrine' => array(
-        'driver' => array(
-            'ScnSocialAuth-Entity' => array(
-                'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
-                'paths' => array(__DIR__ . '/../src/ScnSocialAuthDoctrineORM/Entity'),
-            ),
-            'orm_default' => array(
-                'drivers' => array(
-                    'ScnSocialAuthDoctrineORM\Entity'  => 'ScnSocialAuth-Entity'
-                )
-            )
-        )
-    ),
     'service_manager' => array(
         'factories' => array(
             'ScnSocialAuth-ModuleOptions' => 'ScnSocialAuthDoctrineORM\Service\ModuleOptionsFactory',
